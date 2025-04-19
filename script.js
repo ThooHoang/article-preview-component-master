105f672c5627a67039efe15a655fc2d44bca9453
Quang Tho Hoang <147547139+HoanTheG@users.noreply.github.com>
2025-04-19 20:47:10 +0200

    refactor: improve code structure and organization

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ document.getElementById("shareIcon").addEventListener('click', function myFuncti
     x.classList.toggle("hidden");
 
 })
+document.getElementById("mobile-share-icon-id").addEventListener('click', function myFunction() {
+    let x = document.getElementById("sharePopUp");
+    x.classList.toggle("hidden");
+
+})
